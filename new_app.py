0bd45ca6a6633121d3997fc6d3bf318103e03a96
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-22 15:15:55 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -1,41 +1,50 @@
-from st_on_hover_tabs import on_hover_tabs
+import os
 import streamlit as st
-st.set_page_config(layout="wide")
+import streamlit.components.v1 as components
 
-st.header("Custom tab component for on-hover navigation bar")
-st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+_RELEASE = True
+
+if _RELEASE:
+
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(root_dir, "frontend/build")
+
+    _on_hover_tabs = components.declare_component(
+        "on_hover_tabs",
+        path = build_dir
+    )
+
+else:
+    _on_hover_tabs = components.declare_component(
+    "on_hover_tabs",
+    url="http://localhost:3001"
+    )
+
+def on_hover_tabs(tabName, iconName, styles=None, default_choice=1, key=None):
+    
+    component_value = _on_hover_tabs(tabName=tabName, iconName=iconName, styles=styles, key=key, default=tabName[default_choice])
+    
+    return component_value
+
+if not _RELEASE:
+    st.subheader("Component that creates tabs corresponding with on hover sidebar")
+    st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True) # Load the on hover side bar css file
 
 
-with st.sidebar:
-    tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
-                         iconName=['dashboard', 'money', 'economy'], default_choice=0)
 
-if tabs =='Dashboard':
-    st.title("Navigation Bar")
-    st.write('Name of option is {}'.format(tabs))
+    with st.sidebar:
+         tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
+                              iconName=['dashboard', 'money', 'economy'], 
+                              key="1") ## create tabs for on hover navigation bar
 
-elif tabs == 'Money':
-    st.title("Paper")
-    st.write('Name of option is {}'.format(tabs))
+    if tabs =='Dashboard':
+        st.title("Navigation Bar")
+        st.write('Name of option is {}'.format(tabs))
 
-elif tabs == 'Economy':
-    st.title("Tom")
-    st.write('Name of option is {}'.format(tabs))
-with st.sidebar:
-        tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
-                             iconName=['dashboard', 'money', 'economy'],
-                             styles = {'navtab': {'background-color':'#111',
-                                                  'color': '#818181',
-                                                  'font-size': '18px',
-                                                  'transition': '.3s',
-                                                  'white-space': 'nowrap',
-                                                  'text-transform': 'uppercase'},
-                                       'tabOptionsStyle': {':hover :hover': {'color': 'red',
-                                                                      'cursor': 'pointer'}},
-                                       'iconStyle':{'position':'fixed',
-                                                    'left':'7.5px',
-                                                    'text-align': 'left'},
-                                       'tabStyle' : {'list-style-type': 'none',
-                                                     'margin-bottom': '30px',
-                                                     'padding-left': '30px'}},
-                             key="1")
+    elif tabs == 'Money':
+        st.title("Paper")
+        st.write('Name of option is {}'.format(tabs))
+
+    elif tabs == 'Economy':
+        st.title("Tom")
+        st.write('Name of option is {}'.format(tabs))
