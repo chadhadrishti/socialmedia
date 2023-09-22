047b4f91081af713aed97542b971df13c1c56e4c
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-22 15:11:37 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -1,34 +1,23 @@
+from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
+st.set_page_config(layout="wide")
 
-# Define CSS styles for the side tabs and hover effect
-side_tab_styles = """
-    .side-tabs {
-        display: flex;
-        flex-direction: column;
-        padding: 10px;
-    }
-    .side-tab {
-        padding: 10px;
-        cursor: pointer;
-        transition: background-color 0.3s;
-    }
-    .side-tab:hover {
-        background-color: #f0f0f0;
-    }
-"""
+st.header("Custom tab component for on-hover navigation bar")
+st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
-# Inject the CSS styles into the Streamlit app
-st.markdown(f'<style>{side_tab_styles}</style>', unsafe_allow_html=True)
 
-# Create a dictionary mapping tab labels to their content
-tab_content = {
-    "Tab 1": "This is the content of Tab 1",
-    "Tab 2": "This is the content of Tab 2",
-    "Tab 3": "This is the content of Tab 3",
-}
+with st.sidebar:
+    tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
+                         iconName=['dashboard', 'money', 'economy'], default_choice=0)
 
-# Create a sidebar for the side tabs
-selected_tab = st.sidebar.radio("Select a tab:", list(tab_content.keys()))
+if tabs =='Dashboard':
+    st.title("Navigation Bar")
+    st.write('Name of option is {}'.format(tabs))
 
-# Display the content of the selected tab
-st.write(tab_content[selected_tab])
+elif tabs == 'Money':
+    st.title("Paper")
+    st.write('Name of option is {}'.format(tabs))
+
+elif tabs == 'Economy':
+    st.title("Tom")
+    st.write('Name of option is {}'.format(tabs))
