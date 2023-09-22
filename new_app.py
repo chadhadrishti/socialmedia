20bd39ef984a97bf3a703a730c036f793eb33731
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-22 15:49:35 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -8,7 +8,7 @@ st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_ht
 
 with st.sidebar:
     tabs = on_hover_tabs(name=['Dashboard', 'Money', 'Economy'], 
-                         iconName=['dashboard', 'money', 'economy'], default_choice=0)
+                         iconName=['dashboard', 'money', 'economy'])
 
 if tabs =='Dashboard':
     st.title("Navigation Bar")
