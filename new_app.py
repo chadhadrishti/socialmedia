5a5e9d6d7395aa632f3a48b460ca327d3040bfbf
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-22 17:50:02 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -7,7 +7,7 @@ st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_ht
 
 
 with st.sidebar:
-    tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
+    tabs = on_hover_tabs(Name=['Dashboard', 'Money', 'Economy'], 
                          iconName=['dashboard', 'money', 'economy'])
 
 if tabs =='Dashboard':
